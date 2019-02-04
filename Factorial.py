88f17e8afb13eb1df903fa38d5c7b3632d8ba395
Cynic1404 <nekhaenok@gmail.com>
2019-02-04 15:09:24 -0800

    Prime numbers

---
+++ Factorial.py
@@ -0,0 +1,8 @@
+def factorial(a):
+    result = 1
+    for i in range (1, a+1):
+        result = result*i
+    print(result)
+
+factorial(int(input("Ente your number ")))
+
--- Star.py
+++ Star.py
@@ -1,20 +1,29 @@
+import os
+
 def diamond(n):
 	cspace = (n - 1) / 2
 	cstar = 1
 	
 	if n % 2 != 0:
 		while cstar < n:
-			print " " * cspace + "*" * (cstar) + "\n"
+			print(" " * int(cspace) + "*" * (int(cstar)) + "\n")
 			cspace -= 1
 			cstar += 2
 		
-		print "*" * n + "\n"
+		print ("*" * n + "\n")
 		while cstar > 0:
-			print " " * (cspace + 1) + "*" * (cstar - 2) + "\n"
+			print (" " * (int(cspace) + 1) + "*" * (cstar - 2) + "\n")
 			cspace += 1
 			cstar -= 2
 	else:
 		return None
+
 	
-a = diamond(13)
+a = diamond(41)
+
+
+file = ("file.txt")
+with open(file, "+r") as file:
+	file.write(a)
+	exit()
 
