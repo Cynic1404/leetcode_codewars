7ba1e53ef6912ad9e0106714ba79d4d8a3387deb
mnekhaenok <mnekhaenok@tesla.com>
2022-07-21 14:54:39 -0700

    valid_parentheses.py

--- leetcode/valid_parentheses.py
+++ leetcode/valid_parentheses.py
@@ -1,46 +1,42 @@
-# def isValid(s):
-#     s=list(s)
-#     dict = {"(":")", "[":"]", "{":"}"}
-#     if len(s)%2!=0:
+# def isValid_bruteforce(s):
+#     s = list(s)
+#     dict = {"(": ")", "[": "]", "{": "}"}
+#     if len(s) % 2 != 0:
 #         return False
 #     else:
-#         for opa in range(len(s)):
-#             if s[0] in dict.values():
-#                 return False
-#             for _ in range(1, len(s)):
-#                     if s[_] == dict[s[0]]:
-#                         s.pop(0)
-#                         _-= 1
-#                         s.pop(_)
-#                         if len(s) == 0:
-#                             return True
-#                         elif len(s) == 1:
-#                             return False
-#                         break
+#         while len(s) > 1:
+#             for _ in range(len(s)):
+#                 if s[_] in dict.values():
+#                     return False
+#                 if len(s) == 1 or len(s) == 2 and s[1] != dict[s[0]]:
+#                     return False
+#                 if len(s) == 0 or len(s) == 2 and s[1] == dict[s[0]]:
+#                     return True
+#                 if s[_ + 1] == dict[s[_]]:
+#                     s.pop(_)
+#                     s.pop(_)
+#                     break
 #         return False
 
 
-# print(isValid("[)"))
-
 def isValid(s):
-    s=list(s)
-    dict = {"(":")", "[":"]", "{":"}"}
-    if len(s) % 2 != 0:
-        return False
-    else:
-        while len(s)>1:
-            for _ in range(len(s)):
-                if s[_] in dict.values():
-                    return False
-                if len(s) == 1 or len(s) == 2 and s[1] != dict[s[0]]:
-                    return False
-                if len(s) == 0 or len(s) == 2 and s[1] == dict[s[0]]:
-                    return True
-                if s[_+1] == dict[s[_]]:
-                    s.pop(_)
-                    s.pop(_)
-                    break
-        return False
+    dict = {"(": ")", "[": "]", "{": "}"}
+    check_list = []
 
+    for _ in s:
+        if len(check_list)==0 and s[0] in dict.values():
+            return False
+        elif len(check_list) == 0:
+            check_list.append(_)
+        elif len(check_list) > 0:
+            if _ == ")" and check_list[-1] == "(":
+                check_list.pop(-1)
+            elif _ == "]" and check_list[-1] == "[":
+                check_list.pop(-1)
+            elif _ == "}" and check_list[-1] == "{":
+                check_list.pop(-1)
+            else:
+                check_list.append(_)
+    return check_list==[]
 
 print(isValid("()"))
