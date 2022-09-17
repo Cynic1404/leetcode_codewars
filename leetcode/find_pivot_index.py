e324cb1b83306c23bf8fca70d5559d51678fc7e0
mnekhaenok <mnekhaenok@tesla.com>
2022-09-16 22:17:44 -0700

    find_pivot_index faster

--- leetcode/find_pivot_index.py
+++ leetcode/find_pivot_index.py
@@ -10,4 +10,16 @@ def pivotIndex(nums):
     else:
         return 0
     return -1
-print(pivotIndex([2,1,-1]))
+
+#faster solution, not mine
+def pivotIndex(nums):
+    sumL = 0
+    sumR = sum(nums)
+    for i in range(len(nums)):
+        sumR -= nums[i]
+        if sumL == sumR:
+            return i
+        sumL += nums[i]
+    return -1
+
+print(pivotIndex([1,7,3,6,5,6]))
