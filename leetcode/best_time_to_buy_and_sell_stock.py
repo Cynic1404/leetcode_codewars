48f66a23dd5c0be8bdece485192f93d75d749dae
mnekhaenok <mnekhaenok@tesla.com>
2022-09-17 14:24:01 -0700

    max profit

--- leetcode/best_time_to_buy_and_sell_stock.py
+++ leetcode/best_time_to_buy_and_sell_stock.py
@@ -1,14 +1,16 @@
 def maxProfit(prices):
     min_price = prices[0]
-    max_profit = 0
-    for i in range(1, len(prices)):
-        if prices[i] - min_price > max_profit:
-            max_profit = prices[i] - min_price
-
-        if min_price > prices[i]:
+    profit = 0
+    for i in range(len(prices)):
+        if prices[i] < min_price:
             min_price = prices[i]
-    return max_profit
+        if prices[i] - min_price > profit:
+            profit = prices[i] - min_price
 
 
+    return profit
 
-print(maxProfit([2, 100, 5, 200, 1,56, 10000]))
+
+print(maxProfit([3,11,4,11,3,9,3,12,2,4,5]))
+
+print(maxProfit([7,1,5,3,6,4]))
