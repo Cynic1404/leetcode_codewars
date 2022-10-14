31c2e022b4610f0b54d4e3b6f6854b18ee26e666
mnekhaenok <mnekhaenok@tesla.com>
2022-10-13 20:26:23 -0700

    longestCommonPrefix better solution

--- leetcode/longestCommonPrefix.py
+++ leetcode/longestCommonPrefix.py
@@ -1,11 +1,22 @@
+# def longestCommonPrefix(strs):
+#     strs = sorted(strs, key=len)
+#     pref = ""
+#     for i in range(len(strs[0])):
+#         for l in strs[1:]:
+#             if strs[0][i] != l[i]:
+#                 return "" if not pref else pref
+#         pref += strs[0][i]
+#     return "" if not pref else pref
+
 def longestCommonPrefix(strs):
     strs = sorted(strs, key=len)
     pref = ""
     for i in range(len(strs[0])):
-        for l in strs[1:]:
-            if strs[0][i] != l[i]:
-                return "" if not pref else pref
-        pref += strs[0][i]
-    return "" if not pref else pref
+        for l in strs:
+            if i==len(l) or strs[0][i] != l[i]:
+                return pref
+        pref+=strs[0][i]
+    return pref
+
 
 print(longestCommonPrefix(["a"]))
