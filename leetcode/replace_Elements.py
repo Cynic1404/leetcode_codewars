310150399dd4279141a44034b5f5ed04a48fa682
mnekhaenok <mnekhaenok@tesla.com>
2022-10-17 21:40:57 -0700

    good solution for replaceElements

--- leetcode/replace_Elements.py
+++ leetcode/replace_Elements.py
@@ -1,18 +1,29 @@
 def replaceElements(arr):
-    max_number = 0
-    min_index = 0
-    max_index = 0
-    while max_index < len(arr) - 1:
-        for i in range(max_index + 1, len(arr)):
-            if arr[i] > max_number:
-                max_number = arr[i]
-                max_index = i
-        for m in range(min_index, max_index):
-            arr[m] = max_number
-        max_number = 0
-        min_index = max_index
+    max_n = arr[-1]
+    for i in range(-2, -len(arr) - 1, -1):
+        if arr[i] > max_n:
+            max_n, arr[i] = arr[i], max_n
+        else:
+            arr[i] = max_n
     arr[-1] = -1
     return arr
+
+
+# def replaceElements(arr):
+#     max_number = 0
+#     min_index = 0
+#     max_index = 0
+#     while max_index < len(arr) - 1:
+#         for i in range(max_index + 1, len(arr)):
+#             if arr[i] > max_number:
+#                 max_number = arr[i]
+#                 max_index = i
+#         for m in range(min_index, max_index):
+#             arr[m] = max_number
+#         max_number = 0
+#         min_index = max_index
+#     arr[-1] = -1
+#     return arr
 
 
 assert replaceElements([17,18,5,4,6,1]) == [18,6,6,6,1,-1]
