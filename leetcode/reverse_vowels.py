95ecfb1a3b28b530727ed93c6c27fe38392824f0
mikhail.nekhaenok <mikhail.nekhaenok@bytedance.com>
2023-03-14 19:31:15 -0700

    reverseVowels2

--- leetcode/reverse_vowels.py
+++ leetcode/reverse_vowels.py
@@ -11,3 +11,21 @@ class Solution:
         for i in range(len(letters)):
             s[indexes[i]]=letters[i]
         return("".join(s))
+
+
+    #memory efficient
+    def reverseVowels2(self, s: str) -> str:
+        s = list(s)
+        start = 0
+        end = len(s) - 1
+        while end > start:
+            if s[start] not in "AaEeIiOoUu":
+                start += 1
+            if s[end] not in "AaEeIiOoUu":
+                end -= 1
+
+            if s[start] in "AaEeIiOoUu" and s[end] in "AaEeIiOoUu":
+                s[start], s[end] = s[end], s[start]
+                start += 1
+                end -= 1
+        return "".join(s)
