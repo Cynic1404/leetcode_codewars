b3b719de76c0f53eb455788f44e8b0af15e269d6
mikhail.nekhaenok <mikhail.nekhaenok@bytedance.com>
2023-11-09 14:13:28 -0800

    maxProfit  to return day's indexes

--- leetcode/best_time_to_buy_and_sell_stock.py
+++ leetcode/best_time_to_buy_and_sell_stock.py
@@ -14,3 +14,37 @@ def maxProfit(prices):
 print(maxProfit([3,11,4,11,3,9,3,12,2,4,5]))
 
 print(maxProfit([7,1,5,3,6,4]))
+
+
+# to return day's indexes
+
+# def maxProfit(prices):
+#     if len(prices) > 1:
+#         res=[]
+#         day_to_buy = 0
+#         day_to_sell = 1
+#         profit = prices[day_to_sell] - prices[day_to_buy]
+#         min_price = prices[day_to_buy]
+#         for i in range(1, len(prices) - 1):
+#             if prices[i] < min_price:
+#                 min_price = prices[i]
+#                 day_to_buy = i
+#             if prices[i + 1] - prices[day_to_buy] > profit:
+#                 profit = prices[i + 1] - prices[day_to_buy]
+#                 day_to_sell = i + 1
+#                 res.append([profit, day_to_buy, day_to_sell])
+#     else:
+#         return 0
+#     if res:
+#         print(res)
+#         max=0
+#         for i in res:
+#             if i[0]>max:
+#                 max = i[0]
+#                 day_to_buy = i[1]
+#                 day_to_sell = i[2]
+#         return (max, day_to_buy, day_to_sell)
+#     elif profit>0:
+#         return (profit, day_to_buy, day_to_sell)
+#     else:
+#         return 0
