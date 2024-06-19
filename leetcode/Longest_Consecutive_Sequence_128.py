652f1ad941014820990e592bfea83f31a1039631
mikhail.nekhaenok <mikhail.nekhaenok@bytedance.com>
2024-06-19 12:08:04 -0700

    longestConsecutive

--- leetcode/Longest_Consecutive_Sequence_128.py
+++ leetcode/Longest_Consecutive_Sequence_128.py
@@ -1,19 +1,19 @@
+
 def longestConsecutive(nums):
-    longest = 0
-    nums = sorted(nums)
-    count = 0
-    for i in range(1, len(nums)):
-        if nums[i] - nums[i - 1] == 1:
-            count += 1
-        elif nums[i] == nums[i - 1]:
-            continue
-        else:
-            count = 0
-        longest = max(longest, count)
-    if nums:
-        return longest + 1
-    else:
+    if not nums:
         return 0
 
+    nums = sorted(nums)
+    longest = 0
+    counter = 1
+    for i in range(1, len(nums)):
+        if nums[i] - nums[i - 1] == 1:
+            counter += 1
+        elif nums[i] != nums[i - 1]:
+            longest = max(longest, counter)
+            counter = 1
+    return max(longest, counter)
+
+assert longestConsecutive(nums=[]) == 0
 assert longestConsecutive(nums=[0,3,2,5,4,6,1,1]) == 7
 assert longestConsecutive(nums = [2,20,4,10,3,4,5]) == 4
