235bd579d710b1d85b687191b64ea26dfcdfb1ff
mikhail.nekhaenok <mikhail.nekhaenok@bytedance.com>
2024-08-19 15:04:07 -0700

    maxArea brute force

--- leetcode/max_area_Container_With_Most_Water_11.py
+++ leetcode/max_area_Container_With_Most_Water_11.py
@@ -1,3 +1,12 @@
+#brute force
+# def maxArea(height):
+#     max_area = 0
+#     for left in range(len(height)):
+#         for right in range(left + 1, len(height)):
+#             area = min(height[right], height[left]) * (right - left)
+#             max_area = max(area, max_area)
+#     return max_area
+
 def maxArea(height):
     max_area = 0
     left, right = 0, len(height) - 1
