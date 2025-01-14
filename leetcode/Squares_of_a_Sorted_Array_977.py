be64889b07fcbf8100b3c82839cf22b384ff173b
mikhail.nekhaenok <mikhail.nekhaenok@bytedance.com>
2025-01-14 11:23:04 -0800

    sortedSquares

--- leetcode/Squares_of_a_Sorted_Array_977.py
+++ leetcode/Squares_of_a_Sorted_Array_977.py
@@ -7,13 +7,15 @@ def sortedSquares(nums):
 # two pointers - slower O(N)
 # def sortedSquares(nums):
 #     left = 0
-#     right = len(nums)-1
-#     res = []
-#     while left<=right:
-#         if nums[left]**2>nums[right]**2:
-#             res.append(nums[left]**2)
-#             left+=1
+#     right = len(nums) - 1
+#     res = [0] * len(nums)
+#     for i in range(len(nums) - 1, -1, -1):
+#         if abs(nums[left]) > abs(nums[right]):
+#             res[i] = nums[left] ** 2
+#             left += 1
 #         else:
-#             res.append(nums[right]**2)
-#             right-=1
-#     return res[::-1]
+#             res[i] = nums[right] ** 2
+#             right -= 1
+#     return res
+
+print(sortedSquares([-4,-1,0,3,10]))
